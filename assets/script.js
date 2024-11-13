c58804beb1d95483dbb88789e38c61b9032dfcd3
DoriaFabio <doriafabio2001@gmail.com>
2024-11-13 19:30:09 +0100

    do structure card with html and add h1 and h5

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 "use strict";
 console.clear();
 
+// Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+// (trovate l’array del team all’interno della cartella in allegato)
+// *Bonus*
+// - Rendere l’esercizio responsive, mandando a capo le card
+// - Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team!)
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
